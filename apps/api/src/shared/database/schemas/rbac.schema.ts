f3cff4d8e027219a1b2b246f1a6aeaaed5444823
adiazt01 <adiaztovar2004@gmail.com>
2026-01-27 09:15:15 -0400

    Add DB migration and schemas for families & RBAC

---
+++ apps/api/src/shared/database/schemas/rbac.schema.ts
@@ -0,0 +1,23 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const roles = sqliteTable('roles', {
+  id: text('id').primaryKey(),
+  name: text('name').notNull(), 
+});
+
+export const modules = sqliteTable('modules', {
+  id: text('id').primaryKey(),
+  name: text('name').notNull().unique(), // censo, inventario, etc.
+});
+
+export const permissions = sqliteTable('permissions', {
+  id: text('id').primaryKey(),
+  name: text('name').notNull().unique(), // create, read, update, delete
+});
+
+export const rolePermissions = sqliteTable('role_permissions', {
+  id: text('id').primaryKey(),
+  roleId: text('role_id').references(() => roles.id),
+  moduleId: text('module_id').references(() => modules.id),
+  permissionId: text('permission_id').references(() => permissions.id),
+});
