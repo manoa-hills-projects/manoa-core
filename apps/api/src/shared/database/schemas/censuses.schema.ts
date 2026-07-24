8e864bbe95b312118ac50bd1ce20a8679f11f36b
adiazt01 <armandodt2004@gmail.com>
2026-07-24 18:59:51 -0400

    fix: import sql en censuses.schema.ts

--- apps/api/src/shared/database/schemas/censuses.schema.ts
+++ apps/api/src/shared/database/schemas/censuses.schema.ts
@@ -1,5 +1,5 @@
 
-import { relations } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { baseColumns } from "./base.schema";
 
